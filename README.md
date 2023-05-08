737aef8e02fd434f0956da9e50163b9890d7725e
koenigsley <koenigsley@gmail.com>
2023-05-08 17:50:16 +0300

    docs: add setting up instructions

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 A simple package for DRF errors formatting
 
-## Installation
+## Setting up
+
+Install this package using pip
 
 ```bash
 pip install djangorestframework-verbose-errors
+```
+
+Register the exception handler
+
+```python
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'rest_framework_verbose_errors.views.exception_handler'
+}
 ```
--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 
 from rest_framework_verbose_errors import __version__
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='djangorestframework-verbose-errors',
     version=__version__,
     license='MIT',
     description='A simple package for DRF errors formatting',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Mikhail Yeremeyev',
     author_email='koenigsley@gmail.com',
