414cd9bce726b0b038adc035b07f63d00acc6174
Restyled.io <commits@restyled.io>
2023-05-08 12:05:50 +0000

    Restyled by yapf

--- tests/views_test.py
+++ tests/views_test.py
@@ -9,51 +9,46 @@ settings.configure()
 
 
 class ViewsTestCase(unittest.TestCase):
+
     def test_exception_handler_returns_correct_response_if_detail_is_str(self):
         exc = APIException("error")
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            "errors": [
-                {
-                    "field": None,
-                    "messages": ["error"],
-                }
-            ]
+            "errors": [{
+                "field": None,
+                "messages": ["error"],
+            }]
         }
         self.assertEqual(expected, response.data)
 
-    def test_exception_handler_returns_correct_response_if_detail_is_detail_dict(self):
+    def test_exception_handler_returns_correct_response_if_detail_is_detail_dict(
+            self):
         exc = APIException({"detail": "error"})
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            "errors": [
-                {
-                    "field": None,
-                    "messages": ["error"],
-                }
-            ]
+            "errors": [{
+                "field": None,
+                "messages": ["error"],
+            }]
         }
         self.assertEqual(expected, response.data)
 
     def test_exception_handler_returns_correct_response_if_detail_is_dict_with_list(
-        self,
-    ):
+        self, ):
         exc = APIException(
-            {"name": ["This field is required", "This field can not be null"]}
-        )
+            {"name": ["This field is required", "This field can not be null"]})
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            "errors": [
-                {
-                    "field": "name",
-                    "messages": [
-                        "This field is required",
-                        "This field can not be null",
-                    ],
-                }
-            ]
+            "errors": [{
+                "field":
+                "name",
+                "messages": [
+                    "This field is required",
+                    "This field can not be null",
+                ],
+            }]
         }
         self.assertEqual(expected, response.data)
