bada0fe9e464a445b12a8bc52765add78840c90b
Restyled.io <commits@restyled.io>
2023-05-08 12:05:21 +0000

    Restyled by black

--- tests/views_test.py
+++ tests/views_test.py
@@ -11,43 +11,49 @@ settings.configure()
 
 class ViewsTestCase(unittest.TestCase):
     def test_exception_handler_returns_correct_response_if_detail_is_str(self):
-        exc = APIException('error')
+        exc = APIException("error")
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            'errors': [
+            "errors": [
                 {
-                    'field': None,
-                    'messages': ['error'],
+                    "field": None,
+                    "messages": ["error"],
                 }
             ]
         }
         self.assertEqual(expected, response.data)
 
     def test_exception_handler_returns_correct_response_if_detail_is_detail_dict(self):
-        exc = APIException({'detail': 'error'})
+        exc = APIException({"detail": "error"})
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            'errors': [
+            "errors": [
                 {
-                    'field': None,
-                    'messages': ['error'],
+                    "field": None,
+                    "messages": ["error"],
                 }
             ]
         }
         self.assertEqual(expected, response.data)
 
-    def test_exception_handler_returns_correct_response_if_detail_is_dict_with_list(self):
+    def test_exception_handler_returns_correct_response_if_detail_is_dict_with_list(
+        self,
+    ):
         exc = APIException(
-            {'name': ['This field is required', 'This field can not be null']})
+            {"name": ["This field is required", "This field can not be null"]}
+        )
         context = {}
         response = exception_handler(exc, context)
         expected = {
-            'errors': [
+            "errors": [
                 {
-                    'field': 'name',
-                    'messages': ['This field is required', 'This field can not be null'],
+                    "field": "name",
+                    "messages": [
+                        "This field is required",
+                        "This field can not be null",
+                    ],
                 }
             ]
         }
