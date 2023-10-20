07feee96bd9a34639b37331705630cf8b0eab6c1
koenigsley <koenigsley@gmail.com>
2023-10-20 20:08:05 +0300

    test: add list detail and tuple detail tests

--- tests/views_test.py
+++ tests/views_test.py
@@ -11,35 +11,70 @@ from rest_framework_verbose_errors.views import exception_handler
 
 class ViewsTestCase(unittest.TestCase):
     def test_exception_handler_returns_correct_response_if_detail_is_str(self):
-        exc = APIException('error')
+        exc = APIException('You are not authenticated')
         context = {}
         response = exception_handler(exc, context)
         expected = {
             'errors': [
                 {
                     'field': None,
-                    'messages': ['error'],
-                }
-            ]
+                    'messages': ['You are not authenticated'],
+                },
+            ],
         }
         self.assertEqual(expected, response.data)
 
-    def test_exception_handler_returns_correct_response_if_detail_is_detail_dict(self):
-        exc = APIException({'detail': 'error'})
+    def test_exception_handler_returns_correct_response_if_detail_is_dict_which_contains_detail_key(self):
+        exc = APIException({'detail': 'Permisssion denied'})
         context = {}
         response = exception_handler(exc, context)
         expected = {
             'errors': [
                 {
                     'field': None,
-                    'messages': ['error'],
-                }
-            ]
+                    'messages': ['Permisssion denied'],
+                },
+            ],
         }
         self.assertEqual(expected, response.data)
 
-    def test_exception_handler_returns_correct_response_if_detail_is_dict_with_list(self):
-        exc = APIException({'name': ['This field is required', 'This field can not be null']})
+    def test_exception_handler_returns_correct_response_if_detail_is_list(self):
+        exc = APIException(['Invalid name', 'Invalid age'])
+        context = {}
+        response = exception_handler(exc, context)
+        expected = {
+            'errors': [
+                {
+                    'field': None,
+                    'messages': ['Invalid name', 'Invalid age'],
+                },
+            ],
+        }
+        self.assertEqual(expected, response.data)
+
+    def test_exception_handler_returns_correct_response_if_detail_is_tuple(self):
+        exc = APIException(('Invalid name', 'Invalid age'))
+        context = {}
+        response = exception_handler(exc, context)
+        expected = {
+            'errors': [
+                {
+                    'field': None,
+                    'messages': ['Invalid name', 'Invalid age'],
+                },
+            ],
+        }
+        self.assertEqual(expected, response.data)
+
+    def test_exception_handler_returns_correct_response_if_detail_is_dict_which_contains_list_value(self):
+        exc = APIException(
+            {
+                'name': [
+                    'This field is required',
+                    'This field can not be null',
+                ],
+            },
+        )
         context = {}
         response = exception_handler(exc, context)
         expected = {
@@ -47,7 +82,7 @@ class ViewsTestCase(unittest.TestCase):
                 {
                     'field': 'name',
                     'messages': ['This field is required', 'This field can not be null'],
-                }
-            ]
+                },
+            ],
         }
         self.assertEqual(expected, response.data)
